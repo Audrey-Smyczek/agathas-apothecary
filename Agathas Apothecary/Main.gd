31d06c35cafcd63164eae19c202e5f8b421e17e8
Audrey Smyczek <asmyczek@macalester.edu>
2023-10-18 20:29:33 -0500

    Duplicates made, not sure how to access them

--- Agathas Apothecary/Main.gd
+++ Agathas Apothecary/Main.gd
@@ -3,18 +3,37 @@ extends Node2D
 # Input function from: https://docs.godotengine.org/en/stable/tutorials/inputs/mouse_and_input_coordinates.html
 # Get Position: https://ask.godotengine.org/11729/how-do-i-get-the-objects-coordinates-needed-for-movement
 
+#var ingred1
+
+#func _init():
+#	var ingred1 = get_node("Bowl1")
+var newIngred
 
 
 func _input(event): # Mouse in viewport coordinates.
 	#Vector2 armPos(0.0,0.0)
-	if event is InputEventMouseButton: 
+	if event is InputEventMouseButton:
 		print("Mouse Click/Unclick at: ", event.position)
 		if (event.position.x >= 370 && event.position.x <= 470 && event.position.y >= 870 && event.position.y <= 940):
-			print("bowl 1")
+			print("bowl 1\n")
+			var newIngred1 = get_node("Lavender").duplicate()
+			# the parent can be get_tree().get_root() or some other node
+			get_tree().get_root().add_child(newIngred1)
+			# ownership is different, I think it's not the same root as the root node
+			newIngred1.set_owner(get_tree().get_edited_scene_root())
+			#print(newIngred1)
+			newIngred1.add_to_group("Ingreds")
+			newIngred = newIngred1
+			newIngred.add_to_group("Ingreds")
+			
+			# CAUSES AN ERROR, GOOD TO SEE VARIABLES
+			#print("New" + newIngred)
 		if (event.position.x >= 500 && event.position.x <= 610 && event.position.y >= 900 && event.position.y <= 1000):
-			print("bowl 2")
+			print("bowl 2\n")
+			var newIngred2 = get_node("Bowl2").duplicate()
+			print(newIngred2)
 		if (event.position.x >= 640 && event.position.x <= 770 && event.position.y >= 900 && event.position.y <= 1000):
-			print("bowl 3")
+			print("bowl 3\n")
 		if (event.position.x >= 805 && event.position.x <= 950 && event.position.y >= 890 && event.position.y <= 960):
 			print("bowl 4")
 		if (event.position.x >= 580 && event.position.x <= 800 && event.position.y >= 700 && event.position.y <= 900):
--- Agathas Apothecary/bowl.gd
+++ Agathas Apothecary/bowl.gd
@@ -7,10 +7,10 @@ extends StaticBody2D
 #func _input(event): # Mouse in viewport coordinates.
 	#Vector2 armPos(0.0,0.0)
 	
-func _input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			print("A Click!")
+#func _input_event(viewport, event, shape_idx):
+#	if event is InputEventMouseButton:
+#		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+#			print("A Click!")
 	
 #func _input_event(viewport, event, shape_idx):
 #	if event is InputEventMouseButton: 
