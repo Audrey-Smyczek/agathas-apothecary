2b5f72acee822a18117f09aafd15300805a50e6a
Audrey Smyczek <asmyczek@macalester.edu>
2023-10-21 15:58:46 -0500

    new test interaction created

--- Agathas Apothecary/test_interaction.gd
+++ Agathas Apothecary/test_interaction.gd
@@ -1,5 +1,6 @@
-extends Node2D
+extends Area2D
 
 func _on_Area2D_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if (event is InputEventScreenTouch or event is InputEventMouseButton) and event.is_pressed():
 		print(name)
+		print("Test")
