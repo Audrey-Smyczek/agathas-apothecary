3022c8b19ddf18ba5905d58fabd53748bb571653
Cat Martins <cmartins@macalester.edu>
2023-10-21 15:50:39 -0500

    cleaning up

--- Agathas Apothecary/Node2D.gd
+++ Agathas Apothecary/Node2D.gd
@@ -1,15 +1,32 @@
-extends Node2D
+extends Area2D
 
-func _on_area_2d_mouse_entered():
+
+func on_mouse_enter():
+	print("mouse entered")
 	
-	set_scale(Vector2(4.5, 4.5))
+	
+func on_mouse_exit():
+	print("mouse exited")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	connect("mouse_entered", on_mouse_enter())
+	connect("mouse_exited", on_mouse_exit())
 	
 	pass # Replace with function body.
 
-
-func _on_area_2d_mouse_exited():
-	
-	set_scale(Vector2(2, 2))
-	
-	pass # Replace with function body.
-
+#
+#func _on_area_2d_mouse_entered():
+#
+#	set_scale(Vector2(4.5, 4.5))
+#
+#	pass # Replace with function body.
+#
+#
+#func _on_area_2d_mouse_exited():
+#
+#	set_scale(Vector2(2, 2))
+#
+#	pass # Replace with function body.
+#
