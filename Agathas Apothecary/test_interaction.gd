68394e38947848bed0c3409b630c831f78826a75
Audrey Smyczek <asmyczek@macalester.edu>
2023-10-21 16:05:46 -0500

    test interaction gone, test control made

--- Agathas Apothecary/test_interaction.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-func _on_Area2D_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if (event is InputEventScreenTouch or event is InputEventMouseButton) and event.is_pressed():
-		print(name)
-		print("Test")
