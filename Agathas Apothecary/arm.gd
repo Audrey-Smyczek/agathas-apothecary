70e065d35fcc5f82431f9d7e3e382074da316070
Daniela Martinez <dmartine@macalester.edu>
2023-10-24 16:00:36 -0500

    Can detect individual bowls
    Paul helped us to get each individual bowls when hovered, but it doesn't tell us when it's clicked for florals. Work this out so it can be more dynamic and not hardcoded.

--- Agathas Apothecary/arm.gd
+++ Agathas Apothecary/arm.gd
@@ -4,14 +4,14 @@ extends CanvasLayer
 
 
 
-func _input(event): # Mouse in viewport coordinates.
-	#Vector2 armPos(0.0,0.0)
-	
-	if event is InputEventMouseButton: 
-		
-		print("Mouse Click/Unclick at: ARM")
-#	elif event is InputEventMouseMotion:
-#		print("Mouse Motion at: ", event.position)
+#func _input(event): # Mouse in viewport coordinates.
+#	#Vector2 armPos(0.0,0.0)
+#
+#	if event is InputEventMouseButton: 
+#
+#		print("Mouse Click/Unclick at: ARM")
+##	elif event is InputEventMouseMotion:
+##		print("Mouse Motion at: ", event.position)
 
    # Print the size of the viewport.
 	#print("Viewport Resolution is: ", get_viewport().get_visible_rect().size)
--- Agathas Apothecary/bowl.gd
+++ Agathas Apothecary/bowl.gd
@@ -1,15 +1,25 @@
-extends StaticBody2D
-
+extends Node2D
 # Imput function from: https://docs.godotengine.org/en/stable/tutorials/inputs/mouse_and_input_coordinates.html
 
+#@export var clickableObject: CollisionObject2D
 
 #func _input(event): # Mouse in viewport coordinates.
-	#Vector2 armPos(0.0,0.0)
+#	if event is InputEventMouseButton && event.is_pressed():
+#		print("this is a bowl click", event)
+#		#Vector2armPos(0.0,0.0)
 	
-#func _input_event(viewport, event, shape_idx):
-#	if event is InputEventMouseButton:
-#		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-#			print("A Click!")
+
+#func _ready():
+#	clickableObject.input_event.connect(handle_click)
+
+
+#This allows us to see the independent bowls when hovered over.
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	print("This is from _on_area_2d_input_event", self, event)
+	if event is InputEventMouseButton: #This isn't running, ask lucy for help.
+		print("It was a button", event.button_index, event.pressed, event.is_pressed())
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			print("A Click!")
 	
 #func _input_event(viewport, event, shape_idx):
 #	if event is InputEventMouseButton: 
@@ -21,6 +31,3 @@ extends StaticBody2D
 
    # Print the size of the viewport.
 	#print("Viewport Resolution is: ", get_viewport().get_visible_rect().size)
-
-
-
