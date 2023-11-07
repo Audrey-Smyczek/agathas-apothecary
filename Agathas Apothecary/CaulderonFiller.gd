47d2bbc7ec81c6ecf6bc836d0b3390b5105f40ce
Cat Martins <103918801+catmartz@users.noreply.github.com>
2023-11-07 15:12:51 -0600

    Fixing Yellow Debugger Errors

--- Agathas Apothecary/CaulderonFiller.gd
+++ Agathas Apothecary/CaulderonFiller.gd
@@ -9,7 +9,3 @@ func mixColorsInside():
 		#color will mix in
 	pass 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
--- Agathas Apothecary/bowl.gd
+++ Agathas Apothecary/bowl.gd
@@ -21,7 +21,7 @@ extends Node2D
 
 
 #This allows us to see the independent bowls when hovered over.
-func _on_area_2d_input_event(viewport, event, shape_idx):
+func _on_area_2d_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 #		print("It was a button at:", event.button_index, event.pressed, event.is_pressed())
 		print(ingredInfo.get_Name())
