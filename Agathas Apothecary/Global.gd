1055814db96d67634bd99c19dff4d60c716b7bf4
Cat Martins <103918801+catmartz@users.noreply.github.com>
2023-11-09 19:25:36 -0600

    Cleaning and adding background

--- Agathas Apothecary/Global.gd
+++ Agathas Apothecary/Global.gd
@@ -3,7 +3,9 @@ extends Node
 var curIngredName : String
 var curIngredTexture : Texture
 var curIngredColor : Color
-var mixedColor : Color
+
 var bowlClicked : bool = false
+
 var ingredClicked : bool = false
-var spriteClicked : bool = false
+
+var mixedColor : Color
