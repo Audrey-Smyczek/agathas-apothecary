dedba9c9008529ba8dd8db02410bd472fa6f2255
Cat Martins <103918801+catmartz@users.noreply.github.com>
2023-11-09 21:36:43 -0600

    trying to add text label

--- Agathas Apothecary/bowl.gd
+++ Agathas Apothecary/bowl.gd
@@ -5,6 +5,7 @@ extends Node2D
 
 
 func _ready():
+
 	pass
 
 
@@ -12,9 +13,14 @@ func _ready():
 # selected. Also set's global bool to show that a bowl was clicked
 func _on_area_2d_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed and Global.pathDone:
+		
+#		$BowlTextLabel.text = ingredInfo.getName()
+		
 		var getCurName = ingredInfo.get_Name()
 		Global.curIngredName = getCurName
 		print(Global.curIngredName, " bowl clicked!")
+		
+#		$BowlTextLabel.text = getCurName
 		
 		var getCurTex = ingredInfo.get_Texture()
 		Global.curIngredTexture = getCurTex
