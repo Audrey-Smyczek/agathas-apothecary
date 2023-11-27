a3a0ea9f8b0878cf3a8b7d47f913f7839a15b4b6
Cat Martins <103918801+catmartz@users.noreply.github.com>
2023-11-27 15:22:34 -0600

    created funciton to calculate hsv color. Doesn't work as predicted. overall program still works

--- Agathas Apothecary/Main/Main.gd
+++ Agathas Apothecary/Main/Main.gd
@@ -8,6 +8,7 @@ func _ready():
 	var cauldron = get_node("CauldColor")
 	cauldron.modulate = Color(0,0,0,0)
 	Global.welcomeButton = get_node("Welcome")
+	rgb_to_hsv(45, 215, 0)
 
 
 func _process(_delta):
@@ -41,4 +42,55 @@ func mixing():
 func _on_ingred_sprite_texture_changed():
 	Global.bowlClicked = false
 #	pass
+
+
+# Python3 program change RGB Color 
+# Model to HSV Color Model 
+func rgb_to_hsv(r0, g0, b0): 
+	# declaring vars
+	var h
+	var s
+	var v
+	# R, G, B values are divided by 255 
+	# to change the range from 0..255 to 0..1: 
+	var r = r0 / 255
+	var g = g0 / 255
+	var b = b0 / 255
+
+	# h, s, v = hue, saturation, value 
+	var cmax = max(r, g, b) # maximum of r, g, b 
+	var cmin = min(r, g, b) # minimum of r, g, b 
+	var diff = cmax-cmin	 # diff of cmax and cmin. 
+
+	# if cmax and cmax are equal then h = 0 
+	if cmax == cmin: 
+		h = 0
+	
+	# if cmax equal r then compute h 
+	elif cmax == r: 
+		h = (60 * ((g - b) / diff) + 360) % 360
+
+	# if cmax equal g then compute h 
+	elif cmax == g: 
+		h = (60 * ((b - r) / diff) + 120) % 360
+
+	# if cmax equal b then compute h 
+	elif cmax == b: 
+		h = (60 * ((r - g) / diff) + 240) % 360
+
+	# if cmax equal zero 
+	if cmax == 0: 
+		s = 0
+	else: 
+		s = (diff / cmax) * 100
+
+	# compute v 
+	v = cmax * 100
+	
+	print (h, s, v)
+	
+	# testing:
+	# print(rgb_to_hsv(45, 215, 0)) 
+	# print(rgb_to_hsv(31, 52, 29)) 
+	# print(rgb_to_hsv(129, 88, 47)) 
 
