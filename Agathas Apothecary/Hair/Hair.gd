240b902037565f02a68e4ef2edbdd792092809b4
Cat Martins <103918801+catmartz@users.noreply.github.com>
2023-12-15 08:14:25 -0600

    Refactored Hair scene

--- Agathas Apothecary/Hair/Hair.gd
+++ Agathas Apothecary/Hair/Hair.gd
@@ -1,10 +1,10 @@
+"""
+When the hair is clicked, it sets the global variable of hairClicked to be true. 
+This is a trigger for other functions to run.
+"""
+
 extends Node2D
 
-func _ready():
-	pass
-
-# When the hair is clicked, it sets the global variable of hairClicked to be
-# true. This triggers a function in Main to run.
 func _on_area_2d_input_event(_viewport, event, _shape_idx):
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+	if event is InputEventMouseButton  and event.pressed:
 		Global.hairClicked = true
